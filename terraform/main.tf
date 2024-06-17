7947cbf48789eb58599dd22310c0d9e1423b85cb
z <zack.dean@gmail.com>
2024-06-17 18:07:51 -0400

    replacing my_ with nonthing.

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,29 +4,29 @@ provider "aws" {
 }
 
 # Create vpc 
-resource "aws_vpc" "my_ewm_vpc" {
+resource "aws_vpc" "ewm_vpc" {
 	cidr_block = "10.0.0.0/16"
 }
 
 # create subnet
-resource "aws_subnet" "my_ewm_subent" {
-	vpc_id = aws_vpc.my_ewm_vpc.id
+resource "aws_subnet" "ewm_subent" {
+	vpc_id = aws_vpc.ewm_vpc.id
 	cidr_block = "10.0.1.0/24"
 	availability_zone = "us-east-1a"
 }
 
 # create ig 
-resouce "aws_internet_gateway" "my_ewm_igw" {
-	vpc_id = aws_vpc.my_ewm_vpc.id
+resouce "aws_internet_gateway" "ewm_igw" {
+	vpc_id = aws_vpc.ewm_vpc.id
 }
 
 # Create a routing table 
 resource "aws_route_table" "ewm_route_table" {
-	vpc_id = aws_vpc.my_ewm_vpc.id
+	vpc_id = aws_vpc.ewm_vpc.id
 
   	route {
     		cidr_block = "0.0.0.0/0"
-    		gateway_id = aws_internet_gateway.my_igw.id
+    		gateway_id = aws_internet_gateway.igw.id
   }
 }
 
