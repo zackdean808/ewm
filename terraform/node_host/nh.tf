f91cfb3e53b287d710c8410f21eaf2830c546dcb
z <zack.dean@gmail.com>
2024-06-18 12:14:06 -0400

    adding in lines for node and ansible hosts

---
+++ terraform/node_host/nh.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
+
+# Reference the shared resources
+data "terraform_remote_state" "shared" {
+  backend = "local"
+
+  config = {
+    path = "./terraform.tfstate"
+  }
+}
+
+# Create the second EC2 instance
+resource "aws_instance" "node_host" {
+  ami             = "ami-0c55b159cbfafe1f0"  # Change to your preferred AMI
+  instance_type   = "t2.micro"
+  subnet_id       = data.terraform_remote_state.shared.outputs.subnet_id
+  security_groups = [data.terraform_remote_state.shared.outputs.security_group_id]
+
+  tags = {
+    Name = "node_host"
+  }
+}
+
